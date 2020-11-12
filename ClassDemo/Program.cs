7381077b67e94e97ecec9060a69ed1db410540e2
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2020-11-12 11:41:32 +0100

    Add project files.

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person yannick = new Person();
+            yannick.FirstName = "Yannick";
+            yannick.LastName = "Beterams";
+            Person.PeopleCount++;
+            yannick.Show();
+
+            Person yannick1 = new Person()
+            {
+                FirstName = "Yannick",
+                LastName = "Beterams"
+            };
+            Person.PeopleCount++;
+            yannick1.Show();
+
+            var joeri = new Person();
+            joeri.FirstName = "Joeri";
+            joeri.LastName = "Pardon";
+            Person.PeopleCount++;
+            joeri.Show();
+
+            var joeri1 = new Person()
+            {
+                FirstName = "Joeri",
+                LastName = "Pardon"
+            };
+            Person.PeopleCount++;
+            joeri1.Show();
+
+
+        }
+    }
+    public class Person
+    {
+        public static int PeopleCount = 0;
+        public string FirstName;
+        public string LastName;
+
+        public void Show()
+        {
+            Console.WriteLine($"Welcome tot the C# course {FirstName} {LastName}.");
+        }
+    }
+}
